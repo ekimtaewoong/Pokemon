b699b3df229908d88bc8313b02ab936cc18e7400
ekimtaewoong <erickimtaewoong@gmail.com>
2022-04-09 15:35:44 -0700

    Initial commit

---
+++ Project/src/main/java/com/example/project/ButtonPlane.java
@@ -0,0 +1,24 @@
+package com.example.project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+public class ButtonPlane extends Application {
+    @Override
+    public void start(Stage primaryStage)
+    {
+        StackPane pane = new StackPane(new Button("OK"));
+        Scene scene = new Scene(pane, 200, 50);
+        primaryStage.setTitle("Button in a pane");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
