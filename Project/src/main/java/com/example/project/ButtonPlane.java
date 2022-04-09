960bc4636c9a86147df21bb42b8888f3173bb137
ekimtaewoong <erickimtaewoong@gmail.com>
2022-04-09 16:11:18 -0700

    Update ButtonPlane.java

--- Project/src/main/java/com/example/project/ButtonPlane.java
+++ Project/src/main/java/com/example/project/ButtonPlane.java
@@ -21,5 +21,5 @@ public class ButtonPlane extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    //no way this is real
+    //i better get a good grade
 }
